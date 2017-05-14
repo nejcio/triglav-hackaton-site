835ed6365d7e976614fdedb48ed7cff745782100
wrcx <nejc.krajnik@dlabs.si>
2017-05-14 15:07:57 +0200

    sdasda

--- resources/views/app2.blade.php
+++ resources/views/app2.blade.php
@@ -1,1 +1,29 @@
-<!DOCTYPE html><html><head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,minimal-ui"><meta name=apple-mobile-web-app-capable content=yes><meta name=apple-mobile-web-app-status-bar-style content=black><link rel=stylesheet href=https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cosmo/bootstrap.min.css integrity=sha384-h21C2fcDk/eFsW9sC9h0dhokq5pDinLNklTKoxIZRUn3+hvmgQSffLLQ4G4l2eEr crossorigin=anonymous><link href=https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css rel=stylesheet integrity=sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN crossorigin=anonymous><script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCApDxpsbDYUMTzqCj85TVRbxHZmruTj8U"></script><script src=http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/src/infobox.js></script><title>Lanceout</title><link rel=canonical href=https://quickblox.github.io/quickblox-javascript-sdk/samples/webrtc><link href=/css/styles.css rel=stylesheet></head><body><script src=/js/cordova.js></script><div id=app></div><script src=https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js></script><script src=https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js></script><script src=https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min.js></script><script src=https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js></script><script src=https://cdnjs.cloudflare.com/ajax/libs/quickblox/2.3.4/quickblox.min.js></script><script src=/js/e516af91da2ffdd61c1d.main.js></script></body></html>
+<!DOCTYPE html>
+<html>
+<head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,minimal-ui"><meta name=apple-mobile-web-app-capable content=yes><meta name=apple-mobile-web-app-status-bar-style content=black><link rel=stylesheet href=https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cosmo/bootstrap.min.css integrity=sha384-h21C2fcDk/eFsW9sC9h0dhokq5pDinLNklTKoxIZRUn3+hvmgQSffLLQ4G4l2eEr crossorigin=anonymous><link href=https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css rel=stylesheet integrity=sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN crossorigin=anonymous><script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCApDxpsbDYUMTzqCj85TVRbxHZmruTj8U"></script>
+
+<title>Lanceout</title>
+<link rel=canonical href=https://quickblox.github.io/quickblox-javascript-sdk/samples/webrtc><link href=/css/styles.css rel=stylesheet>
+</head>
+<body>
+<script src=/js/cordova.js></script>
+<style>
+    #main-app {
+        display: block;
+        margin:auto;
+        width: 400px;
+        height: 700px;
+        border: 1px solid #686868;
+    }
+</style>
+<a href="/" id="demo" class="btn btn-primary" style="margin: 30px 30px 0;"> Home</a>
+<script>
+document.getElementById("demo").onclick = function() {myFunction()};
+function myFunction() {
+    window.location.replace('/')
+}
+</script>
+<div id="main-app">
+  <div id=app></div>
+</div>
+<script src=https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js></script><script src=https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js></script><script src=https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min.js></script><script src=https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js></script><script src=https://cdnjs.cloudflare.com/ajax/libs/quickblox/2.3.4/quickblox.min.js></script><script src=/js/e516af91da2ffdd61c1d.main.js></script></body></html>
